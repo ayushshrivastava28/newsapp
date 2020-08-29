aeec4cd9286b44dc69e6122dbdd63e4660200b13
ayushshrivastava28 <ayushshrivastava28@gmail.com>
2020-08-29 16:55:23 +0530

    first

---
+++ src/config/rest_consfig.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'in';
+export const category = 'general';
+export const _api_key = '544bd610c94e4f548f757dd483f58b10';
